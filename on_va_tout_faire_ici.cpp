db42af1a95b2071e4dc8fcc2cf3308fd3b544c28
aymer31 <aymeric.moncla@gmail.com>
2025-10-23 11:49:32 +0200

    first file commit

---
+++ on_va_tout_faire_ici.cpp
@@ -0,0 +1,12 @@
+﻿// on_va_tout_faire_ici.cpp : définit le point d'entrée de l'application.
+//
+
+#include "on_va_tout_faire_ici.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ on_va_tout_faire_ici.h
@@ -0,0 +1,8 @@
+﻿// on_va_tout_faire_ici.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
